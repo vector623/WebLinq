3879b3ee53759b6c559869e30af0946bf280dee8
David Gallmeier <dgallmeier@gmail.com>
2018-02-09 14:41:31 -0500

    Initial commit
    Adding library to help pull web requests via functional programming

---
+++ README.md
@@ -0,0 +1,24 @@
+# WebLinq
+
+this class library is meant to aid functional developers interacting with online web apis.  c#'s standard enumerators
+sometimes don't provide enough functionality for web requests, where you often don't know how many subsequent calls to
+enumerate through until the first request has returned.  sometimes, as in the case of amazon's marketplace web service 
+(2013 version, which is latest to date), you won't know how many subsequent pages of results need to be pulled until 
+you're done.
+
+## enumerators
+
+initially adding the AggregateUntil() method, which will accept a seed value (which should be a page result), a func()
+informing the method how to pull subsequent requests, as well as another func() informing the method when to stop.
+
+because i usually won't know how many subsequent web calls are required, i will setup a very large number range to loop
+through, using Enumerator.Range().  however, i need a way to cancel out of additional iterations of the web request 
+after i'm done pulling data. 
+
+so i wrote this so that i could setup iterations of web calls using C#'s linq, but i needed a way to tell linq when to 
+ stop looping.  while C#'s LINQ has a reduce() equivalent (aggregate), it is incompatible with the linq takewhile()
+method.
+
+## todo
+add a console project as a demo and include some mock functions to imitate web requests to the google adwords api and
+the amazon marketplace web service api
---
+++ WebLinq/WebLinq.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebLinq
+{
+    public static class WebLinq
+    {
+        /// <summary>
+        /// This is a variant of c#'s Aggregate(), which takes a function which will end enumeration given a specified
+        /// condition.  This is useful for a lot of web api pulls, which often yield records in batches and you won't
+        /// know how many batches to execute until after the first pull.  This enumerator should be able to replace a
+        /// lot of messy do-while loops that rely on a lot of stateful variables.
+        /// 
+        /// Usually, you will want to pass in a very large number range (using Enumerator.Range()) as the source, but
+        /// accumulate your results into a page object (or something else that contains your next query params and also
+        /// your aggregating collection).
+        /// </summary>
+        /// <param name="source">collection to be iterated over</param>
+        /// <param name="seed">some page result entity, with the results collection and something like a next token or
+        /// total result count value, to be used later in the untilFunc() method</param>
+        /// <param name="func">put your web pull here and return an updated/concatenated page result object</param>
+        /// <param name="untilFunc">a func telling this method when to stop looping (meaning, no more results to fetch)
+        /// </param>
+        /// <typeparam name="TSource"></typeparam>
+        /// <typeparam name="TAccumulate"></typeparam>
+        /// <returns>a fully filled out page result object</returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        public static TAccumulate AggregateUntil<TSource, TAccumulate>(this IEnumerable<TSource> source, 
+            TAccumulate seed, Func<TAccumulate, TSource, TAccumulate> func, Func<TAccumulate,bool> untilFunc) 
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source));
+            }
+
+            if (func == null)
+            {
+                throw new ArgumentNullException(nameof(func));
+            }
+            TAccumulate result = seed;
+            foreach (TSource element in source)
+            {
+                result = func(result, element);
+                if (untilFunc(result))
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
